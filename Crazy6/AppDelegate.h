0ef291765e0e4319acd85c39287d37022e8f8cdc
tobykentrunzhi <396367738@qq.com>
2015-10-26 20:45:46 +0800

    Initial Commit

---
+++ Crazy6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crazy6
+//
+//  Created by Arron on 10/26/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
